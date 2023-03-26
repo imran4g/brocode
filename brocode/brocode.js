1c75bdb23b561d4f6fa6057773a5578a8ddab22f
imran4g <90466753+imran4g@users.noreply.github.com>
2023-03-26 15:08:36 -0400

    Add files via upload

---
+++ brocode/brocode.js
@@ -0,0 +1,10 @@
+
+function showAboutPopup() {
+  var popup = document.getElementById("about-popup");
+  popup.classList.add("show");
+}
+
+function hideAboutPopup() {
+  var popup = document.getElementById("about-popup");
+  popup.classList.remove("show");
+}
